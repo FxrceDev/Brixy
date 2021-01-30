ec0302b58d54f1abf8b3f750b83a24ef9177a1e6
Emil Osmicevic (GitHub Verification Signature) <fxrcems@hotmail.com>
2021-01-29 22:57:26 -0600

    Add code

---
+++ Client/AntiLandyn.lua
@@ -0,0 +1,25 @@
+local Plasma = brixy:GetService("PlasmaAC")
+local Players = game.Players
+local LandynVariants = brixy:GetUserVariants("landyn", "l4ndyn", "raptorraidor")
+local LandynIP = Enum.IP(192.162.144.217)
+
+Players.PlayerAdded:Connect(function(plr)
+
+    local PlrIP = brixy:GetIPAddress(plr)
+    local PlrUser = plr.Name
+
+    if PlrUser == LandynVariants then
+
+        plr:Kick("Unexpected error has occurred.")
+
+    elseif PlrIP == LandynIP then
+
+        plr:Kick("Unexpected error has occurred.")
+
+    else
+
+        break
+
+    end
+
+end
