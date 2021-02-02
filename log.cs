998dda547e99c4993352ca4d7da619c39bf86f4d
Emil Osmicevic (GitHub Verification Signature) <fxrcems@hotmail.com>
2021-02-01 19:43:48 -0600

    Logger

---
+++ log.cs
@@ -0,0 +1,16 @@
+using midnite;
+using midnite.Analytics;
+using midnite.Services;
+using midnite.Network;
+using midnite.AespaService;
+using midnite.Brixy;
+using midnite.Brixy.Services;
+
+void start()
+{
+
+    midnite.PrintLine("Logging")
+    midnite.Services.Logging::LogAction(asd, lg, 5, nil, host, true, 3)
+    break
+
+}
