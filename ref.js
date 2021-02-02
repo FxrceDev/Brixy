20e1943589713c73c0ccf965f1e13aa9e6d21475
Emil Osmicevic (GitHub Verification Signature) <fxrcems@hotmail.com>
2021-02-02 03:21:25 -0600

    Reference

---
+++ ref.js
@@ -0,0 +1,38 @@
+importScripts()
+import { App } from "Midnite Studios";
+
+App.Reference(decodeURIComponent, devicePixelRatio)
+
+function localeChecker()
+
+    import { AppBehavior } from "Midnite Studios";
+    AppBehavior();
+
+end
+
+define([
+    'ms',
+    'Midnite Studios V3'
+], function(require, factory) {
+    'use strict';
+    
+});
+
+define([
+    'ms_1',
+    'Midnite Studios V3'
+], function(require, factory, nampp) {
+    'use strict';
+    
+});
+
+function createLib()
+
+    localeChecker()
+    DeferredPermissionRequest:DOMImplementation(function integer()
+
+        require(app),
+
+    {end,
+
+end})
