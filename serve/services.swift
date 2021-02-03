4c15aa1bca197a393db0eedecbce19b4c43e9b70
Emil Osmicevic (GitHub Verification Signature) <fxrcems@hotmail.com>
2021-02-02 18:09:12 -0600

    Services

---
+++ serve/services.swift
@@ -0,0 +1,34 @@
+for dsd in nxt {
+
+    do {
+        
+        protocol CreateMsg {
+        
+            CreateServiceTable()
+            
+        }
+
+    } catch exception {
+        
+        if exception == "nil" or nil or "" or 0 {
+            
+            func fix(exception, rq) -> Type {
+                
+                repeat {
+                    
+                    until 0:
+                    if let 0 = optional {
+                        
+                        break:
+
+                    }
+
+                } while retry
+
+            }
+
+        }
+
+    }
+    
+}
