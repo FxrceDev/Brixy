045a128465086a8fae0941fcee489d06d4d40413
Emil Osmicevic (GitHub Verification Signature) <fxrcems@hotmail.com>
2021-02-02 18:01:15 -0600

    Audio FX

---
+++ .audio/AudioDispersion.cs
@@ -0,0 +1,36 @@
+using Midnite;
+using Midnite.Audio;
+using Midnite.Audio.Dispersion;
+using Midnite.Refactor;
+
+void StartDispersion() {
+
+    Midnite.PrintLine("Begin Dispersion Effect");
+    private audio = audio.new Midnite.Audio:GetPlayingSounds(index);
+
+    when audio.Playing == true then {
+
+        public async Func() {
+
+            local Dispersed = Midnite.Audio.Dispersion:BeginDispersion();
+            if (Dispersion.Children.Count >= 0) {
+
+                code        DispersionEffect        0       0       0       endTable();
+                code        DispersionEffect        3       9       2       endTable();
+
+                format^
+                $end
+
+            }
+
+        }
+
+    }
+
+}
+
+repeat wait(0) until Midnite.Audio.Playing == true then { 
+
+    StartDispersion();
+
+}
---
+++ Frostbite/frostbite.c
@@ -0,0 +1,13 @@
+#define <rs.h>
+#define <system.h>
+#define <brx.h>
+#define <engine.h>
+
+void function() {
+
+    if (brx.h.Enabled == nil or false): {
+        break
+        retry();
+    }
+
+}
